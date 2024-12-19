9dc04f9aa17c7cfb2525735e74acb3d43ef58159
Felipe Mariano <felipemariano99@gmail.com>
2024-12-19 01:02:07 -0300

    feat(notifications): add payment mailing notification

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -9,15 +11,30 @@ export class PaymentsService {
     apiVersion: '2024-12-18.acacia',
   });
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE) private readonly notificationsService: ClientProxy,
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
       payment_method: 'pm_card_visa',
       automatic_payment_methods: { enabled: true, allow_redirects: 'never' },
+    });
+
+    const formattedAmount = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(amount);
+
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: `Your payment of ${formattedAmount} has completed successfully.`,
     });
 
     return paymentIntent;
