c6704a4bb3fed2a62bd1b55df3dc44945bb13eb4
Felipe Mariano <felipemariano99@gmail.com>
2024-12-31 18:15:56 -0300

    feat(*): production deployment

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -8,6 +8,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { LocalStrategy } from '../strategies/local.strategy';
 import { JwtStrategy } from '../strategies/jwt.strategy';
+import { HealthModule } from '@app/common/health/health.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { JwtStrategy } from '../strategies/jwt.strategy';
         TCP_PORT: Joi.number().required(),
       }),
     }),
+    HealthModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
